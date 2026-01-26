4a9bbf85eadc3b4c619cadfc01bb4df26f2ddec5
Jitesh Saini <148760926+Jitesh-Saini@users.noreply.github.com>
2026-01-26 18:05:01 +0530

    Time: 126 ms (5.42%) | Memory: 95 MB (5.70%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        map<int,int>m;
+        for (auto x : nums) {
+            m[x]++;
+        }
+        for (auto x : m) {
+            if (x.second > 1) {
+                return true;
+            }
+          
+        }
+        return false;
+    }  
+};
